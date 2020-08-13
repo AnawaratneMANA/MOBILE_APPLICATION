a26601b3d9f56cdbb97110a198e17194ba9f9e00
SalithaEkanayaka123 <ekanayakasalitha1@gmail.com>
2020-08-13 11:09:42 +0530

    Submission for viva

--- app/src/main/java/com/example/homeworktackingsoftware/Notification.java
+++ app/src/main/java/com/example/homeworktackingsoftware/Notification.java
@@ -10,5 +10,6 @@ public class Notification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
