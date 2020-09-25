2b8b64506df6e10255c85e5f8befa387ea6a6750
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2020-09-25 19:44:33 +0530

    change dbHelper

--- app/src/main/java/com/example/finalmadproject/Database/DatabaseHelper.java
+++ app/src/main/java/com/example/finalmadproject/Database/DatabaseHelper.java
@@ -553,4 +553,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         else return false;
     }
 
+    public Cursor readAlltasks(){
+        String query = "SELECT * FROM " + TABLE2_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }
+
 }
