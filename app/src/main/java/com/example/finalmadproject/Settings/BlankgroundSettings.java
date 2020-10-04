00abd2fd7d867a939364456db8e93740192dc659
SalithaEkanayaka123 <ekanayakasalitha1@gmail.com>
2020-10-04 10:55:00 +0530

    Small change to side bar

---
+++ app/src/main/java/com/example/finalmadproject/Settings/BlankgroundSettings.java
@@ -0,0 +1,81 @@
+package com.example.finalmadproject.Settings;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.example.finalmadproject.Database.DatabaseHelper;
+import com.example.finalmadproject.R;
+import com.example.finalmadproject.TanPart.T_MainActivity;
+import com.example.finalmadproject.TanPart.profile;
+public class BlankgroundSettings extends Fragment {
+
+
+    Button profilee;
+    Button Alarm;
+    Button signout;
+    ImageView settingsTravel;
+    DatabaseHelper mydb;
+    ImageView logo;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+        View v2 = inflater.inflate(R.layout.fragment_background, container, false);
+        profilee = v2.findViewById(R.id.ProfilesettingsButton2);
+        Alarm = v2.findViewById(R.id.AlarmsettingsButton2);
+        signout = v2.findViewById(R.id.Signout);
+        settingsTravel = v2.findViewById(R.id.setIcon);
+
+        //mydb = new DatabaseHelper(this.getContext());
+        logo = v2.findViewById(R.id.company_logo);
+        /**
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), CommonLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settingsTravel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity_sl.class);
+                startActivity(intent);
+            }
+        });
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), T_MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        profilee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), profile.class);
+                startActivity(intent);
+
+            }
+        });
+**/
+
+        return v2;
+    }
+}
